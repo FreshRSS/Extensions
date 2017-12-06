efc6c31b5c05cd767665131f99ac6654d26d6b9a
Kevin Papst <kpapst@gmx.net>
2017-12-06 22:42:00 +0100

    adjusted german translation

--- xExtension-CustomCSS/i18n/de/ext.php
+++ xExtension-CustomCSS/i18n/de/ext.php
@@ -3,6 +3,6 @@
 return array(
 	'custom_css' => array(
 		'write_css' => 'Benutzerspezifische CSS Regeln',
-		'permission_problem' => 'Dein CSS file ist nicht schreibbar, bitte ändere die Berechtigungen von %s'
+		'permission_problem' => 'Deine CSS Datei ist nicht beschreibbar, bitte ändere die Berechtigungen von %s'
 	),
 );
--- xExtension-CustomJS/i18n/de/ext.php
+++ xExtension-CustomJS/i18n/de/ext.php
@@ -3,6 +3,6 @@
 return array(
 	'custom_css' => array(
 		'write_css' => 'Benutzerspezifische Javascript Regeln',
-		'permission_problem' => 'Dein JS file ist nicht schreibbar, bitte ändere die Berechtigungen von %s'
+		'permission_problem' => 'Deine JS Datei ist nicht beschreibbar, bitte ändere die Berechtigungen von %s'
 	),
 );
