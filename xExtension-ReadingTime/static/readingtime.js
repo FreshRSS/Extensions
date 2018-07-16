aa0153c0ccab63f1b4f4b9a84fa613cdaed50131
Alexandre Alapetite <alexandre@alapetite.fr>
2018-07-16 10:25:05 +0200

    Pass jshint check

--- xExtension-ReadingTime/static/readingtime.js
+++ xExtension-ReadingTime/static/readingtime.js
@@ -1,4 +1,6 @@
-(function reading_time () {
+/* globals $ */
+
+(function reading_time() {
     'use strict';
 
     var reading_time = {
@@ -13,24 +15,24 @@
     init: function() {
         var flux_list = $("div[id^='flux']");
 
-        for(var i = 0; i < flux_list.length; i++) {
+        for (var i = 0; i < flux_list.length; i++) {
             //console.log($("div[id^='flux']")[i], "Length (words): ", reading_time.flux_words_count($("div[id^='flux']")[i]))
 
-            reading_time.flux = $("div[id^='flux']")[i]
+            reading_time.flux = $("div[id^='flux']")[i];
 
-            reading_time.words_count = reading_time.flux_words_count($("div[id^='flux']")[i]) // count the words
-            reading_time.reading_time = reading_time.calc_read_time(reading_time.words_count, 300) // change this number (in words) to your prefered reading speed
+            reading_time.words_count = reading_time.flux_words_count($("div[id^='flux']")[i]); // count the words
+            reading_time.reading_time = reading_time.calc_read_time(reading_time.words_count, 300); // change this number (in words) to your prefered reading speed
 
             if (document.body.clientWidth <= 840) { // in mobile mode, the feed name is not visible (there is only the favicon)
                 // add the reading time right before article's title
                 // in that case, [Time] - [Title] format is used instead of a "|" (as it looks better and doesn't take much more space)
                 if ( $("#" + reading_time.flux.id + " ul.horizontal-list li.item.title a")[0].textContent.substring(0,(reading_time.reading_time + 'm - ').length) != reading_time.reading_time + 'm - ' ) {
-                                        $("#" + reading_time.flux.id + " ul.horizontal-list li.item.title a")[0].textContent = reading_time.reading_time + 'm - ' + $("#" + reading_time.flux.id + " ul.horizontal-list li.item.title a")[0].textContent
+                                        $("#" + reading_time.flux.id + " ul.horizontal-list li.item.title a")[0].textContent = reading_time.reading_time + 'm - ' + $("#" + reading_time.flux.id + " ul.horizontal-list li.item.title a")[0].textContent;
                     }
                 } else {
                 // add the reading time just after the feed name
-                if ( $("#" + reading_time.flux.id + " ul.horizontal-list li.item.website")[0].textContent.substring(1,(reading_time.reading_time + 'm|').length +1) != reading_time.reading_time + 'm|' ) {
-                    $("#" + reading_time.flux.id + " ul.horizontal-list li.item.website")[0].childNodes[0].childNodes[2].textContent = reading_time.reading_time + 'm| ' + $("#" + reading_time.flux.id + " ul.horizontal-list li.item.website")[0].childNodes[0].childNodes[2].textContent
+                if ( $("#" + reading_time.flux.id + " ul.horizontal-list li.item.website")[0].textContent.substring(1, (reading_time.reading_time + 'm|').length + 1) != reading_time.reading_time + 'm|' ) {
+                    $("#" + reading_time.flux.id + " ul.horizontal-list li.item.website")[0].childNodes[0].childNodes[2].textContent = reading_time.reading_time + 'm| ' + $("#" + reading_time.flux.id + " ul.horizontal-list li.item.website")[0].childNodes[0].childNodes[2].textContent;
                 }
             }
 
@@ -54,21 +56,21 @@
     },
 
     calc_read_time : function calc_read_time(wd_count, speed) {
-        reading_time.read_time = Math.round(wd_count/speed)
-        if (reading_time.read_time === 0) {reading_time.read_time = '<1'}
+        reading_time.read_time = Math.round(wd_count/speed);
+        if (reading_time.read_time === 0) { reading_time.read_time = '<1'; }
         //console.log('Reading time: ', reading_time.read_time)
-        return reading_time.read_time
+        return reading_time.read_time;
     },
     };
 
 
     function add_load_more_listener() {
-        reading_time.init()
+        reading_time.init();
         document.body.addEventListener('freshrss:load-more', function (e) {
-            reading_time.init()
-        })
+            reading_time.init();
+        });
     }
 
-    document.addEventListener('DOMContentLoaded', add_load_more_listener)
+    document.addEventListener('DOMContentLoaded', add_load_more_listener);
 
 }());
