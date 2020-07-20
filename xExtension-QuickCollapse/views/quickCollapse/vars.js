ad61438c3d26f312d85c7d9da2668c8816996939
Marien Fressinaud <dev@marienfressinaud.fr>
2020-07-20 11:22:25 +0200

    Add Quick Collapse extension (#71)
    * Add Quick Collapse extension
    
    * Remove async for JS scripts
    
    * Homogeneize const declarations in script

---
+++ xExtension-QuickCollapse/views/quickCollapse/vars.js
@@ -0,0 +1,7 @@
+var quick_collapse_vars = {
+    icon_url_in: "<?= $this->icon_url_in ?>",
+    icon_url_out: "<?= $this->icon_url_out ?>",
+    i18n: {
+        toggle_collapse: "<?= $this->i18n_toggle_collapse ?>",
+    },
+};
