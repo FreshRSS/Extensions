4837e976e214e2dba8fec270e8ea9db3b15af41e
math-gh <>
2023-09-06 18:11:35 +0200

    fix const/var quick_collapse_vars
    I do not understand why const does not work

--- xExtension-QuickCollapse/views/quickCollapse/vars.js
+++ xExtension-QuickCollapse/views/quickCollapse/vars.js
@@ -1,7 +1,7 @@
 /* exported quick_collapse_vars */
 
 // eslint-disable-next-line no-unused-vars
-const quick_collapse_vars = {
+var quick_collapse_vars = {
 	icon_url_in: '<?= $this->icon_url_in ?>',
 	icon_url_out: '<?= $this->icon_url_out ?>',
 	i18n: {
