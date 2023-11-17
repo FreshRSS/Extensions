c9fe963ee0e9d45be87a14ae17c8d4b5b99ac63d
shinemoon <shinemoon@foxmail.com>
2023-11-17 11:03:17 +0800

    add ColorfulList

---
+++ xExtension-ColorfulList/extension.php
@@ -0,0 +1,9 @@
+<?php
+
+class ColorfulListExtension extends Minz_Extension {
+
+    public function init():void {
+        Minz_View::appendStyle($this->getFileUrl('style.css', 'css'));
+        Minz_View::appendScript($this->getFileUrl('script.js', 'js'),'','','');       
+    }
+}
