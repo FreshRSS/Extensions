da42608eb878f036cd25dbfe4aa70b03cb65741d
shinemoon <shinemoon@foxmail.com>
2023-11-17 11:07:54 +0800

    remove unused css

--- xExtension-ColorfulList/extension.php
+++ xExtension-ColorfulList/extension.php
@@ -3,7 +3,6 @@
 class ColorfulListExtension extends Minz_Extension {
 
     public function init():void {
-        Minz_View::appendStyle($this->getFileUrl('style.css', 'css'));
         Minz_View::appendScript($this->getFileUrl('script.js', 'js'),'','','');       
     }
 }
