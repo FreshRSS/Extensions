d2e6028f8c7526fec9ceffc7e3406cc0341fb389
Paris Jones <32224673+parispjones@users.noreply.github.com>
2023-11-21 14:57:38 -0500

    Update ReadingTime Extension for Safari Support (#171)
    * Update readingtime.js
    
    Change from 'min-width' to hardcoded 40px
    
    * Remove minWidth
    
    * Bump version number
    
    ---------
    
    Co-authored-by: maTh <1645099+math-GH@users.noreply.github.com>

--- xExtension-ReadingTime/static/readingtime.js
+++ xExtension-ReadingTime/static/readingtime.js
@@ -27,8 +27,7 @@
 
 				const li = document.createElement('li');
 				li.setAttribute('class', 'item date');
-				li.style.width = 'min-content';
-				li.style.minWidth = '40px';
+				li.style.width = "40px";
 				li.style.overflow = 'hidden';
 				li.style.textAlign = 'right';
 				li.style.display = 'table-cell';
