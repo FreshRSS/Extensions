a09223b41bb795ff50cbac4012de4c0df6307e33
iClaud <shinemoon@users.noreply.github.com>
2023-11-26 07:17:09 +0800

    [Fix] for xExtension-ColorfulList: JavaScript error on pages that have no feed articles #187 (#188)
    * fix for issue 183
    
    * info update
    
    * Update version
    
    * fix for ticket#187
    
    * refine strict
    
    * strict syntax refine
    
    * refine
    
    * refine
    
    * para refine
    
    * further improvement for initial colorization
    
    * eslint fix
    
    * conner case escape of colorize
    
    * eslint format issue
    
    * use strict
    
    * make fix-all (whitespace details)
    
    ---------
    
    Co-authored-by: shinemoon <shinemoon@foxmail.com>
    Co-authored-by: Alexandre Alapetite <alexandre@alapetite.fr>

--- xExtension-ColorfulList/static/script.js
+++ xExtension-ColorfulList/static/script.js
@@ -1,29 +1,27 @@
 'use strict';
-
-document.addEventListener('DOMContentLoaded', function () {
+window.onload = function () {
 	// Initial Colorize for situation where 'no new item changes triggered later' (https://github.com/FreshRSS/Extensions/issues/183)
 	colorize();
-
-	// Insert entry monitor
+	// Insert entry monitor for autoloading list
 	monitorEntry(colorize);
-});
-
-function monitorEntry(monitorCallback) {
-	const targetNode = document.getElementById('stream');
-	const config = { attributes: false, childList: true, subtree: false };
-	const callback = function (mutationsList, observer) {
-		for (const mutation of mutationsList) {
-			if (mutation.type === 'childList') {
-				monitorCallback(mutationsList);
+	function monitorEntry(monitorCallback) {
+		const targetNode = document.getElementById('stream');
+		const config = { attributes: false, childList: true, subtree: false };
+		const callback = function (mutationsList, observer) {
+			for (const mutation of mutationsList) {
+				if (mutation.type === 'childList') {
+					monitorCallback(mutationsList);
+				}
 			}
+		};
+		const observer = new MutationObserver(callback);
+		if (targetNode) {
+			observer.observe(targetNode, config);
 		}
-	};
-	const observer = new MutationObserver(callback);
-	observer.observe(targetNode, config);
-	// observer.disconnect();
-}
+	}
+};
 
-function colorize() {
+function colorize(mList) {
 	const entry = document.querySelectorAll('.flux_header');
 	entry.forEach((e, i) => {
 		const cl = stringToColour(e.querySelector('.website').textContent) + '12';
