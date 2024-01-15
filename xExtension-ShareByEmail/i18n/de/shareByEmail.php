22859f5e4303d8cfa2720e4a8810c22f6a3358c6
Frans de Jonge <fransdejonge@gmail.com>
2024-01-15 16:28:43 +0100

    remove "E-"

--- xExtension-ShareByEmail/i18n/de/shareByEmail.php
+++ xExtension-ShareByEmail/i18n/de/shareByEmail.php
@@ -10,13 +10,13 @@ return [
 		'form' => [
 			'cancel' => 'Abbrechen',
 			'content' => 'Inhalt',
-			'content_default' => "Hi,\n\nIch glaube dieser Artikel ist interessant für dich!\n\n%s – %s\n\n---\n\nDiese E-Mail wurde von %s gesendet über %s ( %s )",
+			'content_default' => "Hi,\n\nIch glaube dieser Artikel ist interessant für dich!\n\n%s – %s\n\n---\n\nDiese Mail wurde von %s gesendet über %s ( %s )",
 			'send' => 'Senden',
 			'subject' => 'Betreff',
 			'subject_default' => 'Interessanter Artikel für dich!',
 			'to' => 'An',
 		],
-		'intro' => 'Diesen Artikel per E-Mail versenden: “<strong>%s</strong>”',
+		'intro' => 'Diesen Artikel per Mail versenden: “<strong>%s</strong>”',
 		'title' => 'Einen Artikel per Mail teilen.',
 	],
 ];
