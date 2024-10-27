5c610a07aac4e4ad1c919528ba16ea250980bebf
Alexandre Alapetite <alexandre@alapetite.fr>
2024-10-27 14:33:16 +0100

    CustomCSS and CustomJS have been promoted to core extensions https://github.com/FreshRSS/FreshRSS/pull/6267

--- xExtension-CustomJS/i18n/en/ext.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-	'custom_js' => array(
-		'write_js' => 'Additional JS',
-		'permission_problem' => 'Your JS file is not writable, please change the file permissions for %s',
-	),
-);
