bca75e96c488a1a2322907f50782aac5fe8d9aa6
Frans de Jonge <fransdejonge@gmail.com>
2025-01-14 21:44:23 +0100

    Update xExtension-ImageProxy/i18n/fr/ext.php
    Co-authored-by: Alexandre Alapetite <alexandre@alapetite.fr>

--- xExtension-ImageProxy/i18n/fr/ext.php
+++ xExtension-ImageProxy/i18n/fr/ext.php
@@ -5,7 +5,7 @@ return array(
 		'proxy_url' => 'URL du proxy',
 		'scheme_http' => 'Proxy HTTP',
 		'scheme_https' => 'Proxy HTTPS',
-		'scheme_default' => 'Proxy schéma d’URL indéterminé',
+		'scheme_default' => 'Proxy URLs sans protocole',
 		'scheme_include' => 'Inclure http*:// dans l\'URL',
 		'url_encode' => 'Encoder l\'URL'
 	),
