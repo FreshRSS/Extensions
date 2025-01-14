9bd30efee3562d4cba614c5dd6a61dc50c7b16ac
Frans de Jonge <frans@clevercast.com>
2025-01-14 18:37:37 +0100

    add Dutch

---
+++ xExtension-ImageProxy/i18n/nl/ext.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'imageproxy' => array(
+		'proxy_url' => 'Proxy-url',
+		'scheme_http' => 'Http proxyen',
+		'scheme_https' => 'Https proxyen',
+		'scheme_default' => 'Ongespecificeerd url-schema proxyen',
+		'scheme_include' => 'http*:// in url opnemen',
+		'url_encode' => 'Url encoderen'
+	),
+);
