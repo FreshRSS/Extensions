e81ead52f4c7a090b099c53f4b60b76ce38786ab
Frans de Jonge <fransdejonge@gmail.com>
2025-01-15 21:10:51 +0100

    fix typo

--- xExtension-ImageProxy/i18n/nl/ext.php
+++ xExtension-ImageProxy/i18n/nl/ext.php
@@ -5,7 +5,7 @@ return array(
 		'proxy_url' => 'Proxy-url',
 		'scheme_http' => 'Http proxyen',
 		'scheme_https' => 'Https proxyen',
-		'scheme_default' => 'Protocol-relatieve urlproxyen',
+		'scheme_default' => 'Protocol-relatieve url‘s proxyen',
 		'scheme_include' => 'http*:// in url opnemen',
 		'url_encode' => 'Url encoderen'
 	),
