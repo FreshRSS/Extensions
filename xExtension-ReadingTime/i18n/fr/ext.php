6a7203737c51fa079ec4945780dbed59368dd3cb
Alexandre Alapetite <alexandre@alapetite.fr>
2025-04-01 13:27:30 +0200

    i18n fr

---
+++ xExtension-ReadingTime/i18n/fr/ext.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+	'reading_time' => array(
+		'speed' => array(
+			'label' => 'Vitesse de lecture',
+			'help' => 'Paramètre de calcul, dépendant de la méthode',
+			'invalid' => 'La vitesse de lecture doit être supérieure à 0',
+		),
+		'metrics' => array(
+			'label' => 'Méthodes',
+			'help' => 'Méthode de calcul du temps de lecture',
+			'words' => 'Nombre de mots',
+			'letters' => 'Nombre de lettres',
+			'invalid' => 'Méthode non prise en charge',
+		),
+	),
+);
