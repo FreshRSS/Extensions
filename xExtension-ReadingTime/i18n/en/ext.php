bf0f292434637806f50aef4d44f667e2e2841e5f
Alexandre Alapetite <alexandre@alapetite.fr>
2025-04-01 13:22:05 +0200

    Fix i18n en

--- xExtension-ReadingTime/i18n/en/ext.php
+++ xExtension-ReadingTime/i18n/en/ext.php
@@ -7,7 +7,7 @@ return array(
 			'help' => 'Conversion factor to reading time from metrics',
 			'invalid' => 'Reading speed must be greater than 0',
 		),
-		'source' => array(
+		'metrics' => array(
 			'label' => 'Source metrics',
 			'help' => 'Source of reading time calculation',
 			'words' => 'Number of words',
