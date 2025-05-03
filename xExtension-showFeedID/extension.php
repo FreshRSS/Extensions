4ac7c9c6a6c0ba4ad1389747f666fdd7ba9937fb
Alexandre Alapetite <alexandre@alapetite.fr>
2025-05-03 22:37:08 +0200

    Update signature of Minz_Extension::getFileUrl (#310)
    To align with https://github.com/FreshRSS/FreshRSS/pull/7495
    The code still works without this change though.

--- xExtension-showFeedID/extension.php
+++ xExtension-showFeedID/extension.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 final class ShowFeedIdExtension extends Minz_Extension {
 	#[\Override]
 	public function init(): void {
-		Minz_View::appendScript($this->getFileUrl('showfeedid.js', 'js'));
+		Minz_View::appendScript($this->getFileUrl('showfeedid.js'));
 	}
 }
