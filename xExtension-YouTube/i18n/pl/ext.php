31db683bada229780dbf1d299d74a2743123b84d
Inverle <inverle@proton.me>
2025-06-18 00:05:37 +0200

    i18n: pl

---
+++ xExtension-YouTube/i18n/pl/ext.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'yt_videos' => array(
+		'height' => 'Wysokość odtwarzacza wideo',
+		'width' => 'Szerokość odtwarzacza wideo',
+		'show_content' => 'Wyświetlaj zawartość kanałów',
+		'download_channel_icons' => 'Automatycznie używaj ikon kanałów',
+		'use_nocookie' => 'Używaj domeny bez ciasteczek www.youtube-nocookie.com',
+	),
+);
+
