100d7dd688b53126fea4d353cec586a8080d51f8
Jamal <jamalkamaladdin@gmail.com>
2026-09-10 21:45:32 +0400

    [GridView] Add Azerbaijani translation

---
+++ xExtension-GridView/i18n/az/ext.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+	'gridview' => array(
+		'view_mode_name' => 'Şəbəkə görünüşü',
+		'config' => array(
+			'columns' => 'Sütun sayı',
+			'columns_label' => 'Sütun',
+			'columns_help' => 'Şəbəkə görünüşündə göstəriləcək sütunların sayını seçin (2-4). Kiçik ekranlarda bu say avtomatik tənzimlənəcək.',
+			'fetch_og_image' => 'Kiçik şəkillərin yüklənməsi',
+			'fetch_og_image_label' => 'Məqalə səhifələrindən kiçik şəkilləri yüklə',
+			'fetch_og_image_help' => 'Aktivləşdirildikdə, əlavə RSS lentində kiçik şəkli olmayan məqalələrin səhifələrindən Open Graph şəkillərini yükləyəcək. Bu, kartlarda daha yaxşı şəkillərin göstərilməsini təmin edir, lakin səhifənin yüklənməsini ləngidə bilər.',
+			'sort_by_date' => 'Standart sıralama',
+			'sort_by_date_label' => 'Dərc tarixinə görə sırala (ən yenilər əvvəl)',
+			'sort_by_date_help' => 'Aktivləşdirildikdə, standart sıralama dərc tarixinə görə, ən yenilər əvvəl olmaqla təyin edilir. Bu, yalnız bir dəfə tətbiq olunur; sıralamanı yenə də FreshRSS-də əl ilə dəyişə bilərsiniz.',
+			'mobile_menu_button' => 'Yan paneli açıb-bağlama düyməsi',
+			'mobile_menu_button_label' => 'Yan paneli açıb-bağlamaq üçün üzən düymə göstər',
+			'mobile_menu_button_help' => 'Aktivləşdirildikdə, FreshRSS yan panelini açmaq üçün ekranın sol aşağı küncündə üç üfüqi xətdən ibarət üzən menyu düyməsi görünür.',
+			'sticky_nav' => 'Sabit naviqasiya zolağı',
+			'sticky_nav_label' => 'Səhifəni sürüşdürərkən yuxarı naviqasiya zolağını görünən saxla',
+			'sticky_nav_help' => 'Aktivləşdirildikdə, şəbəkə rejimində məqalələri sürüşdürərkən naviqasiya zolağı (oxunmuş/oxunmamış, sevimlilər və s.) yuxarıda sabit qalır.',
+			'usage_title' => 'İstifadə qaydası',
+			'usage_info' => 'Parametrləri yadda saxladıqdan sonra şəbəkə görünüşünü açıb-bağlamaq üçün başlıqdakı şəbəkə işarəsinə (▦) klikləyin və ya klaviaturada "G" düyməsini basın. Məqaləni yeni vərəqdə açmaq üçün istənilən karta klikləyin.',
+		),
+	),
+);
